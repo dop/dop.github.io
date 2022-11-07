0d9357714432ec5b39b0481df08fcac207e60b94
Donatas Petrauskas <donatas.petr@gmail.com>
2022-11-07 23:19:23 +0200

    Many updates.

--- Makefile
+++ Makefile
@@ -1,24 +1,20 @@
 libs := $(wildcard lib/*.el)
 partials := $(wildcard _partials/*)
+styles := $(wildcard assets/css/*)
+bin := /Applications/MacPorts/EmacsMac.app/Contents/MacOS/Emacs
+emacs := $(bin) -batch -Q $(foreach lib,$(libs),-l $(lib))
+
 posts := $(wildcard posts/*.org)
 pages := $(wildcard pages/*.org)
 htmls := $(posts:org=html) $(pages:org=html)
-emacs := emacs -batch -Q $(foreach lib,$(libs),-l $(lib))
 
-all: $(htmls) index.html pages.html
+all: index.html $(htmls)
 
-posts/%.html: posts/%.org $(partials) $(libs)
-	$(emacs) --eval "(export-page \"$<\")"
-
-pages/%.html: pages/%.org $(partials) $(libs)
-	$(emacs) --eval "(export-page \"$<\")"
-
-%.html: %.org $(partials) $(libs)
-	$(emacs) --eval "(export-page \"$<\")"
+%.html: %.org $(partials) $(styles) $(libs)
+	$(emacs) -eval "(export-page \"$<\")"
 
 .PHONEY: clean
 
 clean:
-	rm -f index.html pages.html
-	rm -f pages/*.html
-	rm -f posts/*.html
+	rm -f $(htmls) $(htmls:html:html~)
+	rm -f index.html index.html~
--- lib/export.el
+++ lib/export.el
@@ -4,11 +4,8 @@
 (require 'org)
 (require 'ox-html)
 
-(defvar *export-inlined* t
+(defvar *export-inlined* nil
   "Whether to inline scripts and styles. NIL is useful for development.")
-
-(defvar inline-styles
-  (concat "<style type=\"text/css\">" (org-file-contents "_partials/styles.css") "</style>"))
 
 (defun export-file (filename &optional head postamble)
   (with-current-buffer (find-file-literally filename)
@@ -17,8 +14,35 @@
           (org-html-head-include-default-style nil)
           (org-html-head-include-scripts nil)
           (org-html-postamble postamble)
-          (org-html-head head))
+          (org-html-head head)
+          (org-html-self-link-headlines t))
       (org-html-export-to-html))))
+
+(defun slugify (text)
+  (string-trim (replace-regexp-in-string "[^a-zA-Z0-9_-]+" "-" text) "-" "-"))
+
+(defun org-headline-get-text (data)
+  (let* ((title (plist-get data :title))
+         (texts (org-element-map title '(plain-text) #'substring-no-properties)))
+    (mapconcat #'identity texts " ")))
+
+(defun org-get-file-headlines (file)
+  (with-current-buffer (find-file-noselect file)
+    (org-element-map (org-element-parse-buffer) '(headline)
+      (lambda (headline)
+        (let ((data (elt headline 1)))
+          (list (plist-get data :level) (org-headline-get-text data)))))))
+
+(defun org-html--headline-reference (fn datum &rest args)
+  (cl-case (org-element-type datum)
+    (headline
+     (let* ((title (plist-get (elt datum 1) :title))
+            (texts (org-element-map title '(plain-text) #'substring-no-properties)))
+       (slugify (mapconcat #'identity texts " "))))
+    (t
+     (apply fn datum args))))
+
+(advice-add 'org-html--reference :around #'org-html--headline-reference)
 
 (defun partial-url (exported-filename resource)
   (let ((prefix (cl-loop repeat (1- (length (split-string exported-filename "/"))) concat "../")))
@@ -26,16 +50,168 @@
 
 (defun navigation-html (exported-filename)
   (string-replace "<!-- links -->"
-                  (cl-loop for (name link) in '(("Posts" "index.html") ("Pages" "pages.html"))
+                  (cl-loop for (name link) in '(("Index" "index.html"))
                            concat (concat "<li><a href=\"" (partial-url exported-filename link) "\">" name "</a></li>"))
                   (org-file-contents "_partials/postamble.html")))
 
 (defun export-page (filename)
   (let ((head (if *export-inlined*
-                  (concat "<style type=\"text/css\">" (org-file-contents "_partials/styles.css") "</style>")
-                (concat "<link rel=\"stylesheet\" href=\"" (partial-url filename "_partials/styles.css") "\" type=\"text/css\">"))))
-    (export-file filename head (navigation-html filename))))
+                  (concat "<style type=\"text/css\">" (org-file-contents "assets/css/styles.css") "</style>")
+                (concat "<link rel=\"stylesheet\" href=\"" (partial-url filename "assets/css/styles.css") "\" type=\"text/css\">"))))
+    (export-file filename head
+                 (navigation-html filename))))
 
 (defun export-development-page (filename)
   (export-post filename
-               (concat "<link rel=\"stylesheet\" href=\"styles.css\" type=\"text/css\">")))
+               (concat "<link rel=\"stylesheet\" href=\"" (partial-url filename "assets/css/styles.css") "\" type=\"text/css\">")))
+
+(defun org-babel-expand-noweb-references (&optional info parent-buffer)
+  "Expand Noweb references in the body of the current source code block.
+
+For example the following reference would be replaced with the
+body of the source-code block named `example-block'.
+
+<<example-block>>
+
+Note that any text preceding the <<foo>> construct on a line will
+be interposed between the lines of the replacement text.  So for
+example if <<foo>> is placed behind a comment, then the entire
+replacement text will also be commented.
+
+This function must be called from inside of the buffer containing
+the source-code block which holds BODY.
+
+In addition the following syntax can be used to insert the
+results of evaluating the source-code block named `example-block'.
+
+<<example-block()>>
+
+Any optional arguments can be passed to example-block by placing
+the arguments inside the parenthesis following the convention
+defined by `org-babel-lob'.  For example
+
+<<example-block(a=9)>>
+
+would set the value of argument \"a\" equal to \"9\".  Note that
+these arguments are not evaluated in the current source-code
+block but are passed literally to the \"example-block\"."
+  (let* ((parent-buffer (or parent-buffer (current-buffer)))
+         (info (or info (org-babel-get-src-block-info 'light)))
+         (lang (nth 0 info))
+         (body (nth 1 info))
+         (comment (string= "noweb" (cdr (assq :comments (nth 2 info)))))
+         (noweb-re (format "\\(.*?\\)\\(%s\\)"
+                           (with-current-buffer parent-buffer
+                             (org-babel-noweb-wrap))))
+         (cache nil)
+         (c-wrap
+          (lambda (s)
+            ;; Comment string S, according to LANG mode.  Return new
+            ;; string.
+            (unless org-babel-tangle-uncomment-comments
+              (with-temp-buffer
+                (funcall (org-src-get-lang-mode lang))
+                (comment-region (point)
+                                (progn (insert s) (point)))
+                (org-trim (buffer-string))))))
+         (expand-body
+          (lambda (i)
+            ;; Expand body of code represented by block info I.
+            (let ((b (if (org-babel-noweb-p (nth 2 i) :eval)
+                         (org-babel-expand-noweb-references i)
+                       (nth 1 i))))
+              (if (not comment) b
+                (let ((cs (org-babel-tangle-comment-links i)))
+                  (concat (funcall c-wrap (car cs)) "\n"
+                          b "\n"
+                          (funcall c-wrap (cadr cs))))))))
+         (expand-references
+          (lambda (ref cache)
+            (pcase (gethash ref cache)
+              (`(,last . ,previous)
+               ;; Ignore separator for last block.
+               (let ((strings (list (funcall expand-body last))))
+                 (dolist (i previous)
+                   (let ((parameters (nth 2 i)))
+                     ;; Since we're operating in reverse order, first
+                     ;; push separator, then body.
+                     (push (or (cdr (assq :noweb-sep parameters)) "\n")
+                           strings)
+                     (push (funcall expand-body i) strings)))
+                 (mapconcat #'identity strings "")))
+              ;; Raise an error about missing reference, or return the
+              ;; empty string.
+              ((guard (or org-babel-noweb-error-all-langs
+                          (member lang org-babel-noweb-error-langs)))
+               (error "Cannot resolve %s (see `org-babel-noweb-error-langs')"
+                      (org-babel-noweb-wrap ref)))
+              (_ "")))))
+    (replace-regexp-in-string
+     noweb-re
+     (lambda (m)
+       (with-current-buffer parent-buffer
+         (save-match-data
+           (let* ((prefix (match-string 1 m))
+                  (id (match-string 3 m))
+                  (evaluate (string-match-p "(.*)" id))
+                  (expansion
+                   (cond
+                    (evaluate
+                     ;; Evaluation can potentially modify the buffer
+                     ;; and invalidate the cache: reset it.
+                     (setq cache nil)
+                     (let ((raw (org-babel-ref-resolve id)))
+                       (if (stringp raw) raw (format "%S" raw))))
+                    ;; Return the contents of headlines literally.
+                    ((org-babel-ref-goto-headline-id id)
+                     (org-babel-ref-headline-body))
+                    ;; Look for a source block named SOURCE-NAME.  If
+                    ;; found, assume it is unique; do not look after
+                    ;; `:noweb-ref' header argument.
+                    ((org-with-point-at 1
+                       (let ((r (org-babel-named-src-block-regexp-for-name id)))
+                         (and (re-search-forward r nil t)
+                              (not (org-in-commented-heading-p))
+                              (funcall expand-body
+                                       (org-babel-get-src-block-info t))))))
+                    ;; Retrieve from the Library of Babel.
+                    ((nth 2 (assoc-string id org-babel-library-of-babel)))
+                    ;; All Noweb references were cached in a previous
+                    ;; run.  Extract the information from the cache.
+                    ((hash-table-p cache)
+                     (funcall expand-references id cache))
+                    ;; Though luck.  We go into the long process of
+                    ;; checking each source block and expand those
+                    ;; with a matching Noweb reference.  Since we're
+                    ;; going to visit all source blocks in the
+                    ;; document, cache information about them as well.
+                    (t
+                     (setq cache (make-hash-table :test #'equal))
+                     (org-with-wide-buffer
+                      (org-babel-map-src-blocks nil
+                        (if (org-in-commented-heading-p)
+                            (org-forward-heading-same-level nil t)
+                          (let* ((info (org-babel-get-src-block-info t))
+                                 (ref (cdr (assq :noweb-ref (nth 2 info)))))
+                            (push info (gethash ref cache))))))
+                     (funcall expand-references id cache)))))
+             expansion))))
+     body t t 2)))
+
+(defun file-under (heading)
+  (interactive (list
+                (let* ((index (project-has-file-p (project-current) "index.org"))
+                       (headlines (org-get-file-headlines index))
+                       (level-1-headlines (seq-filter (lambda (item) (= 1 (car item))) headlines)))
+                  (completing-read "Headline: " (mapcar #'cadr level-1-headlines)))))
+  (let* ((file (buffer-file-name))
+         (project (project-current))
+         (root (expand-file-name (project-root project))))
+    (pcase (org-collect-keywords '("TITLE"))
+      (`(("TITLE" ,title))
+       (with-current-buffer (find-file (project-has-file-p project "index.org"))
+         (beginning-of-buffer)
+         (search-forward (concat "* " heading))
+         (newline)
+         (insert (format "** [[file:%s][%s]]" (string-replace root "" file) title))
+         (newline))))))
